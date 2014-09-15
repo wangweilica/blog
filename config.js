26be2e2ee36b3b8812f52d38f0bd954035435942
wangweilica <1017458803@qq.com>
2014-09-15 09:37:24 +0800

    init

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * User: Jack Wang
+ * Date: 14-9-12
+ * Time: 5:37pm
+ */
+module.exports={
+    cookieSecret:'myblog',
+    db:"myblog",
+    connectionstring:"mongodb://Jack:1017458803@127.0.0.1:27017/myblog"
+};
